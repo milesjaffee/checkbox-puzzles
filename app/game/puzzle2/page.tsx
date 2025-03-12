c0cecee60adc2a85a32a8973dbdbffe58659cc0a
Miles Jaffee <mej327@lehigh.edu>
2025-03-12 18:32:24 -0400

    added puzzle #2

--- app/game/puzzle2/page.tsx
+++ app/game/puzzle2/page.tsx
@@ -8,25 +8,43 @@ export default function Page() {
   const [check3, setCheck3] = useState(false);
   const [check4, setCheck4] = useState(false);
   const [check5, setCheck5] = useState(false);
+  const [check6, setCheck6] = useState(false);
+  const [check7, setCheck7] = useState(false);
+  const [check8, setCheck8] = useState(false);
+  const [check9, setCheck9] = useState(false);
 
   const handleCheckboxChange = (whichBox: String) => {
     if (whichBox === 'one') {
       setCheck1(!check1);
-      setCheck4(false);
+        setCheck7(false);
     } else if (whichBox === 'two') {
       setCheck2(!check2);
-      setCheck5(false);
+        setCheck6(false);
     } else if (whichBox === 'three') {
       setCheck3(!check3);
-      setCheck2(false);
+        setCheck1(false);
     } else if (whichBox === 'four') {
       setCheck4(!check4);
+      setCheck5(false);
     } else if (whichBox === 'five') {
       setCheck5(!check5);
-      setCheck1(false);
+        setCheck9(false);
+    } else if (whichBox === 'six') {
+      setCheck6(!check6);
+      setCheck3(false);
+    } else if (whichBox === 'seven') {
+        setCheck7(!check7);
+    } else if (whichBox === 'eight') {
+        setCheck8(!check8);
+        setCheck4(false);
+    } else if (whichBox === 'nine') {
+        setCheck9(!check9);
+        setCheck2(false);
     };
 
     checkDone();
+
+    //8 4 5 9 2 6 3 1 7
 
   };
 
@@ -38,7 +56,7 @@ export default function Page() {
   
     return (
       <section>
-        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Puzzle 1: "Short Demo"</h1>
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Puzzle 2: "Long Demo"</h1>
         <p></p>
         <h2 className="font-semibold text-xl mb-8 tracking-tighter font-italic">Rules:</h2>
         <p>Check all the boxes to solve the puzzle :)</p>
@@ -75,23 +93,30 @@ export default function Page() {
           />
           <label>{" "}Box 4</label></div>
 
-          <div><input
-            type="checkbox"
-            checked={check5}
-            onChange={() => handleCheckboxChange('five')}
-          />
+          <div><input  type="checkbox" checked={check5} onChange={() => handleCheckboxChange('five')}   />  
           <label>{" "}Box 5</label></div>
+        
+          <div><input  type="checkbox" checked={check6} onChange={() => handleCheckboxChange('six')}   />  
+          <label>{" "}Box 6</label></div>
 
+          <div><input  type="checkbox" checked={check7} onChange={() => handleCheckboxChange('seven')}   />  
+          <label>{" "}Box 7</label></div>
+
+          <div><input  type="checkbox" checked={check8} onChange={() => handleCheckboxChange('eight')}   />  
+          <label>{" "}Box 8</label></div>
+
+          <div><input  type="checkbox" checked={check9} onChange={() => handleCheckboxChange('nine')}   />  
+          <label>{" "}Box 9</label></div>
 
         </div>
 
-        {check1 && check2 && check3 && check4 && check5 ?
+        {check1 && check2 && check3 && check4 && check5 && check6 && check7 && check8 && check9 ?
           <div>
             <h2 className="font-semibold text-xl mb-8 tracking-tighter font-italic">Congratulations!</h2>
             <p>You solved the puzzle! </p>
             <a
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#38383877] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                href="/game/puzzle2"
+                href="/game/puzzle3"
                 //target="_blank"
                 //rel="noopener noreferrer"
                 >
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             To progress, select each{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+            <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               checkbox{" "}
             </code>
              on the page.
