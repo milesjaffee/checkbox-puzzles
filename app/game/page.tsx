880318cfd89bf8297097262712ea0107f3fdcb92
Miles Jaffee <mej327@lehigh.edu>
2025-03-12 14:05:46 -0400

    added game directory

---
+++ app/game/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'Projects',
+    description: 'Read my projects.',
+  }
+  
+  export default function Page() {
+    return (
+      <section>
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Checkbox Puzzle Demo</h1>
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Game 1</button>
+      </section>
+    )
+  }
+  
