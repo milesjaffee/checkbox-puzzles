9a58948c4e496482fdb37266e0418f77145c84b0
Miles Jaffee <mej327@lehigh.edu>
2025-08-01 17:04:30 -0400

    rules in puzzlebody component

--- app/[locale]/game/puzzle3/page.tsx
+++ app/[locale]/game/puzzle3/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useI18n, useScopedI18n } from "@/locales/client";
 import CongratulationsMessage from '@/app/components/CongratulationsMessage';
+import PuzzleBody from '@/app/components/PuzzleBody';
 
 export default function Page() {
   const t = useI18n();
@@ -13,7 +14,10 @@ export default function Page() {
   const [done, setDone] = useState(false);
   const [checked, setChecked] = useState<boolean[]>(Array(checkCount).fill(false));
   const [clicks, setClicks] = useState(0);
-  const [order, setOrder] = useState([...Array(checkCount).keys()]);
+
+  const rules = [
+    'limit', 'limit-reset',
+  ];
 
   const onChange = (index: number) => {
     setChecked(prev => {
@@ -25,6 +29,7 @@ export default function Page() {
         const nextIndex = clickOrder[positionInOrder + 1];
         newChecked[nextIndex-1] = false; // Automatically uncheck the next box in order
       }
+      console.log('newChecked', newChecked);
 
       if (newChecked.every((val, idx) => val === finalState[idx])) setDone(true);
       return newChecked;
@@ -44,50 +49,19 @@ export default function Page() {
   };
   
     return (
-      <section>
-        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">{t('puzzles.number', {num: '3'})}: '{t('puzzles.3.title')}'</h1>
-        <p></p>
-        <h2 className="font-semibold text-xl mb-8 tracking-tighter font-italic">{t('puzzles.rules.rules')}</h2>
-        <ol>
-          <li>{t('puzzles.rules.limit', {limit: 
-            <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-            {maxClicks}
-            </code>
-          })}</li>
-          <li>{t('puzzles.rules.limit-reset')}</li>
-        </ol>
-        <p></p>
-        <div className="flex gap-4 items-left flex-col sm:flex-col">
+      <PuzzleBody
+        puzzleNum={3}
+        numBoxes={checkCount}
+        maxClicks={maxClicks}
+        clicks={clicks}
+        //no order here
+        checked={checked}
+        handleChange={handleChange}
+        reset={reset}
+        finalState={finalState}
+        done={done}
+        rules={rules}
+      />
 
-        <h2 className="font-semibold text-xl mt-8 tracking-tighter font-italic">{t('puzzles.puzzle')}</h2>
-        <p></p>
-        {order.map((i) => (
-          <label key={i}>
-            <input
-              type="checkbox"
-              checked={checked[i]}
-              onChange={() => handleChange(i)}
-              disabled={clicks >= maxClicks}
-            />
-            {t('puzzles.box', {num: (i + 1).toString()})}
-          </label>
-        ))}
-
-          <div>
-          <p>{t('puzzles.clicks.clicks', {num: clicks})}</p>
-          <button
-
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#38383877] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            onClick={reset}>{t('puzzles.clicks.reset')}</button>
-          </div>
- 
-
-        </div>
-
-        {done?
-          <CongratulationsMessage puzzleNum={3} />
-          : null}
-
-      </section>
     )
 }
--- app/components/PuzzleBody.tsx
+++ app/components/PuzzleBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, ReactNode } from 'react';
 import { useI18n, useScopedI18n } from "@/locales/client";
 import CongratulationsMessage from "@/app/components/CongratulationsMessage";
 
@@ -13,6 +13,11 @@ interface PuzzleBodyProps {
     reset?: () => void;
     finalState: boolean[];
     done: boolean;
+    rules?: string[];
+
+    shuffleAfter?: number;
+    notes?: string[];
+    decreaseAmount?: number;
 
 }
 
@@ -21,12 +26,17 @@ const PuzzleBody: React.FC<PuzzleBodyProps> = ({
     numBoxes,
     clicks = 0,
     maxClicks = 999,
-    order = [...Array(numBoxes).keys()].map(i => i + 1), // Default order is 1 to numBoxes
+    order = [...Array(numBoxes).keys()].map(i => i), // Default order is 1 to numBoxes
     checked = Array(numBoxes).fill(false), // Default checked state
     handleChange = (index: number) => {},
     reset,
     finalState,
     done,
+    rules = [],
+    shuffleAfter = 999,
+    notes = [],
+    decreaseAmount = 0,
+
 }) => {
     const t = useI18n();
     const puzzleKeys = [
@@ -40,18 +50,57 @@ const PuzzleBody: React.FC<PuzzleBodyProps> = ({
         "puzzles.8.title",
         "puzzles.9.title",
       ] as const;
+
+    const formatRules = (ruleKeys: string[]): ReactNode[] => {
+        return ruleKeys.map((ruleKey) => {
+            if (ruleKey === 'limit-reset') {
+                return t('puzzles.rules.limit-reset');
+            } else if (ruleKey === 'checkall') {
+                return t('puzzles.rules.checkall');
+            } else if (ruleKey === 'uncheck') {
+                return t('puzzles.rules.uncheck');
+            } else if (ruleKey === 'chord') {
+                return t('puzzles.rules.chord');
+            } 
+            
+            else if (ruleKey === 'limit') {
+                return t('puzzles.rules.limit', {
+                    limit: ( <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                            {maxClicks} </code> ), });
+            } else if (ruleKey === 'shuffle') {
+                return t('puzzles.rules.shuffle', {
+                    num: ( <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                            {shuffleAfter? shuffleAfter: 'UNDEFINED'} </code> ), });
+            } else if (ruleKey === 'chord-notes') {
+                return t('puzzles.rules.chord-notes', {
+                    notes1: ( <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                            {notes[0]} </code> ), 
+                    notes2: ( <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                            {notes[1]} </code> ), 
+                            });
+            } else if (ruleKey === 'decrease') {
+                return t('puzzles.rules.decrease', {
+                    amount: ( <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                            {decreaseAmount} </code> ), });
+            }
+            
+            else {
+                return '';
+            }
+        });
+    };
+
     return (
         <section>
-                <h1 className="font-semibold text-2xl mb-8 tracking-tighter">{t('puzzles.number', {num: puzzleNum.toString()})}: '{puzzleKeys[puzzleNum-1]}'</h1>
+                <h1 className="font-semibold text-2xl mb-8 tracking-tighter">{t('puzzles.number', {num: puzzleNum.toString()})}: '{t(puzzleKeys[puzzleNum-1])}'</h1>
                 <p></p>
                 <h2 className="font-semibold text-xl mb-8 tracking-tighter font-italic">{t('puzzles.rules.rules')}</h2>
                 <ol>
-                  <li>{t('puzzles.rules.limit', {limit: 
-                    <code className="bg-black/[.05] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                    {maxClicks}
-                    </code>
-                  })}</li>
-                  <li>{t('puzzles.rules.limit-reset')}</li>
+                  {formatRules(rules).map((rule, index) => (
+                    <li key={index}>
+                        {rule}
+                    </li>
+                  ))}
                 </ol>
                 <p></p>
                 <div className="flex gap-4 items-left flex-col sm:flex-col">
@@ -66,7 +115,7 @@ const PuzzleBody: React.FC<PuzzleBodyProps> = ({
                       onChange={() => handleChange(i)}
                       disabled={clicks >= maxClicks}
                     />
-                    {t('puzzles.box', {num: (i + 1).toString()})}
+                    {t('puzzles.box', {num: (i+1).toString()})}
                   </label>
                 ))}
         
