cf8c99d77540df7053826225c99c8ac7d4ba308d
Miles Jaffee <mej327@lehigh.edu>
2026-08-13 22:53:44 -0400

    renamed middleware to proxt

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { localeKeys } from '@/locales';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Safe exclusions for all API routes and known assets
